0274fb08a2aaef0a25dc9255be9accfa8027d65b
Arturo Espinoza <clonjago@gmail.com>
2016-12-22 23:14:05 -0300

    using Listview with sections headers instead of a scrollview in videos page

--- src/Pages/Videos/containers/Videos.js
+++ src/Pages/Videos/containers/Videos.js
@@ -1,10 +1,11 @@
 'use strict'
 
 import React, { PropTypes } from 'react'
-import {ScrollView, StyleSheet} from 'react-native'
+import {ListView, StyleSheet, Text} from 'react-native'
 import _ from 'lodash'
 
 import VideosList from '../components/VideosList'
+import Colors from '../../../../data/Colors'
 
 export default class Theaters extends React.Component {
   static propTypes = {
@@ -14,48 +15,65 @@ export default class Theaters extends React.Component {
 
   constructor (props) {
     super(props)
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+    });
+    this.state = {
+      dataSource: dataSource.cloneWithRowsAndSections(this._getdataSections(props.viewer))
+    }
+  }
 
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRowsAndSections(this._getdataSections(nextProps.viewer))
+    })
+  }
+
+  _getdataSections (viewer) {
+    return {
+      "Últimos Videos": [viewer.latestVideos],
+      "Cartelera": [viewer.billboardVideos],
+      "Próximos Estrenos": [viewer.comingSoonVideos]
+    };
   }
 
   render () {
     return (
-      <ScrollView
-        style={styles.scrollView}
-      >
-        {this._getLatestVideos()}
-        {this._getBillboardVideos()}
-        {this._getComingSoonVideos()}
-      </ScrollView>
-    )
-  }
-
-  _getLatestVideos() {
-    return(
-      <VideosList
-        videos={this.props.viewer.latestVideos}
+      <ListView
+        style={styles.listView}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+        renderSectionHeader={this._renderSectionHeader}
       />
     )
   }
 
-  _getBillboardVideos() {
-    return(
+  _renderRow (rowData) {
+    if (!rowData || rowData.length === 0) {return null}
+    return (
       <VideosList
-        videos={this.props.viewer.billboardVideos}
+        videos={rowData}
       />
     )
   }
 
-  _getComingSoonVideos() {
-    return(
-      <VideosList
-        videos={this.props.viewer.comingSoonVideos}
-      />
+  _renderSectionHeader(sectionData, sectionID) {
+    return (
+      <Text style={styles.header}>{sectionID}</Text>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1
+  listView: {
+    flex: 1,
+  },
+  header: {
+    padding: 4,
+    paddingLeft: 10,
+    fontSize: 18,
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.8)'
   }
 })
