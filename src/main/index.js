53ae5c5ff6d0ef96de9da0d69629cd4b210f6fec
Arturo Espinoza <clonjago@gmail.com>
2016-12-31 00:52:37 -0300

    set up google analytics for ios and android

--- src/main/index.js
+++ src/main/index.js
@@ -18,6 +18,12 @@ Relay.injectNetworkLayer(
   })
 )
 
+import {
+  GoogleAnalyticsTracker,
+  GoogleTagManager,
+  GoogleAnalyticsSettings
+} from 'react-native-google-analytics-bridge';
+
 import Orientation from 'react-native-orientation'
 
 import CardNavigator from './CardNavigator'
@@ -25,6 +31,15 @@ import DrawerLayout from './DrawerLayout'
 import {getMainAppRoute} from '../../data/routes'
 
 export default class CineHorariosApp extends React.Component {
+
+  constructor(props) {
+    super(props)
+    GoogleAnalyticsSettings.setDispatchInterval(30);
+    GoogleAnalyticsSettings.setDryRun(false);
+    let tracker = new GoogleAnalyticsTracker('UA-89600675-1');
+    tracker.setAppName('Cine Horarios');
+    tracker.trackScreenView('Home');
+  }
 
   componentDidMount () {
     Orientation.lockToPortrait()
