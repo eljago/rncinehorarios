17bc27be62701b3d233913f2eb643682dccac8fa
Arturo Espinoza <clonjago@gmail.com>
2017-01-14 02:56:25 -0300

    made tracker variable a const

--- src/utils/Analytics.js
+++ src/utils/Analytics.js
@@ -4,12 +4,12 @@ import {GoogleAnalyticsTracker} from 'react-native-google-analytics-bridge'
 import config from '../../data/config'
 
 function reportScreenView (screenView) {
-  let tracker = new GoogleAnalyticsTracker(config.analyticsCode)
+  const tracker = new GoogleAnalyticsTracker(config.analyticsCode)
   tracker.trackScreenView(screenView)
 }
 
 function reportEvent (category, action) {
-let tracker = new GoogleAnalyticsTracker(config.analyticsCode)
+  const tracker = new GoogleAnalyticsTracker(config.analyticsCode)
   tracker.trackEvent(category, action);
 }
 
